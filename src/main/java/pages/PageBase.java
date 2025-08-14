256f42fd4aa1129917d06471a4982d5cd70076d2
JlassiHoussem <jlassihoussmeddine@gmail.com>
2025-08-14 18:04:59 +0100

    add product hover menu test

--- src/main/java/pages/PageBase.java
+++ src/main/java/pages/PageBase.java
@@ -3,13 +3,18 @@ package pages;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
 
 public class PageBase {
 
 	protected WebDriver driver;
 	
 	public JavascriptExecutor jse;
+	public Select select;
+	public Actions action;
+	
 	
 	//create constructor
 	public PageBase(WebDriver driver) {
@@ -27,4 +32,6 @@ public class PageBase {
 	public void scrolToBottom() {
 		jse.executeScript("scrollBy(0,2500)");
 	}
+	
+	
 }
